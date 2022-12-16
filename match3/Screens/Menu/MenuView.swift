996aa52d700c8b9a7d307dcbc2cf225ae8561b9d
Fahrenheit <igor.maisyk@gmail.com>
2022-12-16 22:58:18 +0300

    Fix UI layout on iPhone SE

--- match3/Screens/Menu/MenuView.swift
+++ match3/Screens/Menu/MenuView.swift
@@ -3,62 +3,65 @@ import SwiftUI
 struct MenuView: View {
     @EnvironmentObject var settings: Settings
     @EnvironmentObject var viewModel: ViewModel
-
+    
     var body: some View {
         NavigationView {
-            ZStack {
-                Image("background")
-                    .resizable()
-                    .ignoresSafeArea()
+            VStack {
+                TopMenu()
+                    .padding(.horizontal, 20)
                     .navigation(isActive: $viewModel.gameShown) {
                         GameView()
                     }
-                VStack {
-                    TopMenu()
-                    .frame(height: 76)
-                    .padding(.horizontal, 20)
+                
+                Spacer()
+                
 
-                    VStack(spacing: -64.0) {
-                        Image("zeus")
-                        VStack(spacing: 30.0) {
-                            LevelMenu()
-                            VStack(spacing: 20.0) {
-                                Button(
-                                    action: { viewModel.send(update: .startEasy) },
-                                    label: {
-                                        ZStack {
-                                            Image("blueButtonBackground")
-                                            Text("Easy")
-                                                .font(.gameFont(fontSize: 32))
-                                                .foregroundColor(Color.white)
-                                                .padding(.bottom, 8)
-                                        }
-                                    })
-                                
-                                Button(
-                                    action: { viewModel.send(update: .startHard) },
-                                    label: {
-                                        ZStack {
-                                            Image("redButtonBackground")
-                                            Text("Hard")
-                                                .font(.gameFont(fontSize: 32))
-                                                .foregroundColor(Color.white)
-                                                .padding(.bottom, 8)
-                                        }
-                                        
-                                    })
-                                Text("The difficulty level affects the time limit")
-                                    .foregroundColor(Color(hex: "#A93A3A"))
-                                Spacer()
+                VStack(spacing: 24.0) {
+                    LevelMenu()
+                    
+                    Button(
+                        action: { viewModel.send(update: .startEasy) },
+                        label: {
+                            ZStack {
+                                Image("blueButtonBackground")
+                                Text("Easy")
+                                    .font(.gameFont(fontSize: 32))
+                                    .foregroundColor(Color.white)
+                                    .padding(.bottom, 8)
                             }
-                        }
-                    }
+                        })
+                    
+                    Button(
+                        action: { viewModel.send(update: .startHard) },
+                        label: {
+                            ZStack {
+                                Image("redButtonBackground")
+                                Text("Hard")
+                                    .font(.gameFont(fontSize: 32))
+                                    .foregroundColor(Color.white)
+                                    .padding(.bottom, 8)
+                            }
+                            
+                            
+                        })
+                    Text("The difficulty level affects the time limit")
+                        .foregroundColor(Color(hex: "#A93A3A"))
                 }
+                .padding(.bottom)
             }
+            .background(
+                Image("background")
+                .resizable()
+                .ignoresSafeArea()
+                .overlay(Image("zeus")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.bottom, 100)
+                ))
             .overlay(storeView)
         }
     }
-
+    
     @ViewBuilder
     private var storeView: some View {
         if viewModel.storeShown {
